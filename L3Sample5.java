925bde25ac72de618b21d200e5fec4dd439910c1
Mikey <mahatosameer280@gmail.com>
2026-05-26 15:14:36 +0900

    Add files via upload

---
+++ L3Sample5.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class L3Sample5 {
+    public static void main(String[] args)throws IOException {
+        System.out.println("整数を入力してください。");
+        BufferedReader br =
+        new BufferedReader(new InputStreamReader(System.in));
+        String str =br.readLine();
+        int num =Integer.parseInt(str);
+        System.out.println(num + "が入力されました");
+    }
+}
